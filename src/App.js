d6a1d7877ac8a0bfd9221d93d484e4a2c45764c7
unknown <aquib_j@yahoo.com>
2022-10-12 16:07:36 +0530

    Header Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import logo from './images/logo.jpeg'
+import logo from './images/logo.png'
+import header from './images/header-banner.jpg'
+import mobile_cta from './images/CTA_mobile.jpg'
+import cta from './images/CTA-web.jpg'
 
 function App() {
 	return (
@@ -8,7 +11,7 @@ function App() {
 			{/* Header */}
 			<header className="main-header my-3">
 				<div className="main-header-background-image">
-					<img src="https://s3.us-east-2.amazonaws.com/fueler.io-images/brands/tcc/header-banner.jfif" alt="" />
+					<img src={header} alt="" />
 				</div>
 				<div className="container">
 					<div className="main-header-profile mb-4">
@@ -20,13 +23,13 @@ function App() {
 										<img className="rounded-circle mb-4" src={logo} alt="" />
 									</div>
 									<div>
-										<a href="https://www.theclueless.company/" id='website-button' className='btn primary-button px-3 fw-500'>Our Website</a>
+										<a href="https://www.fueler.io" id='website-button' className='btn primary-button px-3 fw-500'>Our Website</a>
 									</div>
 								</div>
-								<h2 className='fw-bold primary-color'>The Clueless Company</h2>
+								<h2 className='fw-bold primary-color'>Fueler</h2>
 								<p className='text-gray fw-500'>We go beyond the conventional methods of consulting, and provides actionable mentoring and strategies to the clueless entrepreneurs.</p>
 
-								<a href="https://www.theclueless.company/" target="_blank" rel="noopener noreferrer" id='website-button-botom' className='btn primary-button px-3 fw-500'>Explore Website</a>
+								<a href="https://www.fueler.io/" target="_blank" rel="noopener noreferrer" id='website-button-botom' className='btn primary-button px-3 fw-500'>Explore Website</a>
 							
 							</div>
 						</div>
@@ -54,32 +57,7 @@ function App() {
 												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149431/fueler-emoji/wgyvec0qmcjctujfslx7.png" alt="" />
 											</div>
 											<div className='card-content'>
-												<p className='mb-0 text-basic fw-500'>Come up with a list of 30 brands who don't have blog on their website</p>
-											</div>
-										</div>
-										<div className='card-tag'>
-											<div className='card-tag-button px-3 py-2'>
-												Marketing
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-						</div>
-
-
-						{/* POW IDea 1 */}
-						<div className="col-md-11 mb-4 pb-2">
-							<div className="card card-green px-3">
-								<div className="card-body">
-									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-										<div className='d-flex justify-content-start flex-wrap align-items-center'>
-											<div className='card-icon my-3 mr-2'>
-												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149685/fueler-emoji/ftojrqu17nf5cjkm6ypb.png" alt="" />
-											</div>
-											<div className='card-content'>
-												<p className='mb-0 text-basic fw-500'>Write a case study on "How Meme marketing is helping brands grow their internet presence?" </p>
+												<p className='mb-0 text-basic fw-500'>Write a Twitter Thread around Freelancing, Writing, Marketing, Product Management, and Video Editor, by mentioning Signup to Fueler as a Call to action</p>
 											</div>
 										</div>
 										<div className='card-tag'>
@@ -93,6 +71,29 @@ function App() {
 
 						</div>
 
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-green px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149685/fueler-emoji/ftojrqu17nf5cjkm6ypb.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>Convert existing Twitter threads posted on <a href='https://twitter.com/fuelerhq'>Fueler</a> handle to the <a href='https://fueler.io/blog'>Fueler blog</a></p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Writing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
 
 						{/* POW IDea 1 */}
 						<div className="col-md-11 mb-4 pb-2">
@@ -104,12 +105,12 @@ function App() {
 												<img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/man-artist_1f468-200d-1f3a8.png" alt="" />
 											</div>
 											<div className='card-content'>
-												<p className='mb-0 text-basic fw-500'>Prepare a social media marketing plan to grow Manasi to 10k followers on Twitter</p>
+												<p className='mb-0 text-basic fw-500'>Shoot an Instagram Reel video about the “Best Website to search for project ideas (mention <a href='https://fueler.io/proof-of-work-ideas'>https://fueler.io/proof-of-work-ideas</a>)”</p>
 											</div>
 										</div>
 										<div className='card-tag'>
 											<div className='card-tag-button px-3 py-2'>
-												Marketing
+												Video Creating
 											</div>
 										</div>
 									</div>
@@ -117,7 +118,6 @@ function App() {
 							</div>
 
 						</div>
-
 
 						{/* POW IDea 1 */}
 						<div className="col-md-11 mb-4 pb-2">
@@ -129,12 +129,12 @@ function App() {
 												<img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/fire_1f525.png" alt="" />
 											</div>
 											<div className='card-content'>
-												<p className='mb-0 text-basic fw-500'>Come up with a list of prospect who is running a business with $100k YOY revenue</p>
+												<p className='mb-0 text-basic fw-500'>Shoot a YouTube video on “How to create a portfolio using Fueler?”</p>
 											</div>
 										</div>
 										<div className='card-tag'>
 											<div className='card-tag-button px-3 py-2'>
-												Marketing
+												Video Creating
 											</div>
 										</div>
 									</div>
@@ -153,7 +153,62 @@ function App() {
 											</div>
 											<div className='card-content'>
 												<p className='mb-0 text-basic fw-500'>
-													Write a Twitter thread on "10 Tips to write a cold email that get response"
+													Shoot a YouTube video on “How to create a customized portfolio using Fueler? (Mention Fueler Bucket)”
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Video Creating
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-pink px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Shoot a YouTube video on “How to create a customized portfolio using Fueler? (Mention Fueler Bucket)”
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Video Creating
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-green px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Write a blog on “How to create a portfolio as a beginner freelancer?” 
 												</p>
 											</div>
 										</div>
@@ -167,13 +222,301 @@ function App() {
 							</div>
 
 						</div>
+
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-teal px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Write a blog on “10 Best Free Portfolio Websites for Creating an Impressive Portfolio (Mention <a href="http://fueler.io/">Fueler.io</a> in 1st)”
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Writing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-peach px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Write a blog on “10 Portfolio Websites to create Copywriting portfolio for free (Mention <a href="http://fueler.io/">Fueler.io</a> in 1st)”
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Writing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-blue px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Write a comparison blog on “Fueler Vs. Wix: Which is the better for creating a portfolio?” 
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Writing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-pink px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Write a comparison blog on “Fueler Vs. Wix: Which is the better for creating a portfolio?” 
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Writing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-green px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Create a YouTube video about “How to use Fueler as a freelance writer?”
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Video Creating
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-teal px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Design an Instagram carousel by explaining “7 Ways to use Fueler portfolio link to get the opportunity” 
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Design
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-peach px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Write a blog on “5 ways to get 1000 seconds of kudos on Fueler” 
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Writing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-blue px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Organize a webinar/workshop for your college campus about Fueler and Tweet about it with pictures 
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Marketing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-pink px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Write a LinkedIn post about Fueler and how someone can use it 
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Writing
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+						{/* POW IDea 1 */}
+						<div className="col-md-11 mb-4 pb-2">
+							<div className="card card-green px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149894/fueler-emoji/cvvhxbsssxjqy26ctcfe.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-basic fw-500'>
+													Design an infographic explaining “How to use Fueler as a freelancer with steps by steps guide?”
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Design
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
 					</div>
 				</div>
 
 			</section>
 
+			
+			
 
 			<footer className='my-4'>
+				<div className=' container-fluid my-5 '>
+					<div className="row">
+						<div className="col-md-12 text-center">
+							<a className='cta' href="https://fueler.io">
+								<img src={cta} className="mw-100" alt="" />
+							</a>
+							<a className='mobile-cta' href="https://fueler.io">
+								<img src={mobile_cta} className="mw-100" alt="" />
+							</a>
+						</div>
+					</div>
+				</div>
+
+
 				<div className='text-center pb-5'>
 					<h5 className='mb-0 py-4'>
 						<a href="https://fueler.io">
